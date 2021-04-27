e40e0ce6c70d29bea91817159200e4d5fa35974b
Py4Rpi <73581490+Py4Rpi@users.noreply.github.com>
2021-04-27 00:53:38 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # token = "1756142897:AAGNvo64_LP2FLSYcU0V-V5giaE9femaW_Q" # superlangbot
-token = "1646801741:AAG5euit4OcfRGXzZJS3Ch5r4x_LI2ZodrQ"  # test
-# token = "1589590128:AAG7he3PT-rPBsuum14tzPnjl42uUMMrFxs"  #LangoSaurus
+# token = "1646801741:AAG5euit4OcfRGXzZJS3Ch5r4x_LI2ZodrQ"  # test
+token = "1589590128:AAG7he3PT-rPBsuum14tzPnjl42uUMMrFxs"  #LangoSaurus
 
 from enum import Enum
 import sqlite3
